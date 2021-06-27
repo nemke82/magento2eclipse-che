97116e4362133374f36658716c8a0bb6a7b7df9c
Nemanja Djuric <ne@nemanja.io>
2021-06-27 19:28:58 +0200

    Update m2-install-solo.sh

--- m2-install-solo.sh
+++ m2-install-solo.sh
@@ -3,16 +3,19 @@
 # Clone M2 repository (dev), for composer comment these lines and use following
 # composer create-project --repository=https://repo.magento.com/ magento/project-community-edition magento2
 
-cd /projects/magento2che &&
-git clone https://github.com/magento/magento2.git && cd magento2 && cp -avr .* /projects/magento2che &&
-cd /projects/magento2che && rm -r -f magento2 &&
+cd /projects/magento2che;
+git clone https://github.com/magento/magento2.git;
+cd magento2 && cp -avr .* /projects/magento2che;
+cd /projects/magento2che && rm -r -f magento2;
 
-mysql -u root -pnem4540 -e 'create database nemanja;' &&
-cd /projects/magento2che && composer install && php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url="https://serverh6w3y3wx-magento2che-server-8080.192.168.34.100.nip.io/" --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis' &&
+mysql -u root -pnem4540 -e 'create database nemanja;';
+cd /projects/magento2che;
+composer install;
+php bin/magento setup:install --db-name='nemanja' --db-user='root' --db-password='nem4540' --base-url="https://serverg7snjxwd-magento2che-server-8080.192.168.34.100.nip.io/" --backend-frontname='admin' --admin-user='admin' --admin-password='adm4540' --admin-email='ne@nemanja.io' --admin-firstname='Nemanja' --admin-lastname='Djuric' --use-rewrites='1' --use-secure='1' --base-url-secure="https://serverg7snjxwd-magento2che-server-8080.192.168.34.100.nip.io/" --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/New_York' --currency='USD' --session-save='redis' &&
 
-n98-magerun2 module:disable Magento_Csp &&
-n98-magerun2 module:disable Magento_TwoFactorAuth &&
-n98-magerun2 setup:upgrade &&
+php bin/magento module:disable Magento_Csp;
+php bin/magento module:disable Magento_TwoFactorAuth;
+php bin/magento setup:upgrade;
 
 yes | php bin/magento setup:config:set -n -q --session-save=redis --session-save-redis-host=127.0.0.1 --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6379 &&
 yes | php bin/magento setup:config:set -n -q --cache-backend=redis --cache-backend-redis-server=127.0.0.1 --cache-backend-redis-db=1 &&
